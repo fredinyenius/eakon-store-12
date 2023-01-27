f14e2b1b9f6958adb3f08c146d760e1a24f88bfa
Fredy Mamani <fredy.mamani.canahua@gmail.com>
2023-01-26 20:23:00 -0500

    contac slice

--- .history/src/redux/store_20230126201637.js
+++ .history/src/redux/store_20230126201637.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  aboutReducer  from "./slices/aboutUsSlice";
 import homeReducer from "./slices/homeSlice";
-import contacReducer from "./slices/contacSlice";
 
 export const store = configureStore({
   reducer: {
